ef65344e78e0034c0aa845535a8d1cb0fe4f685a
CrossDarkrix <crosseddarkrix@gmail.com>
2023-12-14 04:09:54 +0900

    Update install.py

--- install.py
+++ install.py
@@ -4,6 +4,7 @@ import ssl
 import subprocess
 import sys
 import re
+from pip._internal.cli.main import main as pip
 
 try:
     ssl._create_default_https_context = ssl._create_unverified_context
@@ -17,12 +18,14 @@ if not os.path.exists('PyYAML-6.0.1-py3-none-any.whl'):
     with open('PyYAML-6.0.1-py3-none-any.whl', 'wb') as yaml:
         yaml.write(urllib.request.urlopen('https://raw.githubusercontent.com/CrossDarkrix/gamdl-a-Shell/main/PyYAML-6.0.1-py3-none-any.whl').read())
 print('Installing PyYaml.......')
-subprocess.run('pip install PyYAML-6.0.1-py3-none-any.whl', shell=True, stdout=subprocess.PIPE)
+# subprocess.run('pip install PyYAML-6.0.1-py3-none-any.whl', shell=True, stdout=subprocess.PIPE)
+pip(['install', '--upgrade', 'PyYAML-6.0.1-py3-none-any.whl'])
 if not os.path.exists('gamdl-1.9.10.3-py3-none-any.whl'):
     with open('gamdl-1.9.10.3-py3-none-any.whl', 'wb') as gamdl:
         gamdl.write(urllib.request.urlopen('https://raw.githubusercontent.com/CrossDarkrix/gamdl-a-Shell/main/gamdl-1.9.10.3-py3-none-any.whl').read())
 print('Installing gamdl Without Music Video DL Edition.......')
-subprocess.run('pip install gamdl-1.9.10.3-py3-none-any.whl', shell=True, stdout=subprocess.PIPE)
+# subprocess.run('pip install gamdl-1.9.10.3-py3-none-any.whl', shell=True, stdout=subprocess.PIPE)
+pip(['install', '--upgrade', 'gamdl-1.9.10.3-py3-none-any.whl'])
 print('Setting gamdl.............')
 install_path = re.search('/var/mobile/Containers.+', '\n'.join(sys.path)).group()
 os.chdir(install_path)
